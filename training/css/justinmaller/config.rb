1825d53bf0070a5e23a4a9654aee901d54598f15
ben <ben@dreamerslab.com>
2012-02-21 22:02:42 +0800

    rename css, js, img dir

--- training/css/justinmaller/config.rb
+++ training/css/justinmaller/config.rb
@@ -1,4 +1,4 @@
-### 
+###
 # Compass
 ###
 
@@ -31,13 +31,13 @@
 ###
 
 # Per-page layout changes:
-# 
+#
 # With no layout
 # page "/path/to/file.html", :layout => false
-# 
+#
 # With alternative layout
 # page "/path/to/file.html", :layout => :otherlayout
-# 
+#
 # A path which all have the same layout
 # with_layout :admin do
 #   page "/admin/*"
@@ -60,33 +60,35 @@
 # end
 
 # Change the CSS directory
-# set :css_dir, "alternative_css_directory"
+set :css_dir, "css"
+set :http_stylesheets_path, "/css"
 
 # Change the JS directory
-# set :js_dir, "alternative_js_directory"
+set :js_dir, "js"
 
 # Change the images directory
-# set :images_dir, "alternative_image_directory"
+set :images_dir, "img"
+set :http_images_path , "/img"
 
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
   # activate :minify_css
-  
+
   # Minify Javascript on build
   # activate :minify_javascript
-  
+
   # Enable cache buster
   # activate :cache_buster
-  
+
   # Use relative URLs
   # activate :relative_assets
-  
+
   # Compress PNGs after build
   # First: gem install middleman-smusher
   # require "middleman-smusher"
   # activate :smusher
-  
+
   # Or use a different image path
   # set :http_path, "/Content/images/"
 end
